b7cc68bfe77492bba6ca60f8a2e7b05de4af085a
raresmigea <raresini@yahoo.com>
2021-11-06 18:47:05 +0200

    chaged storybook file

--- libs/header/src/lib/header.stories.tsx
+++ libs/header/src/lib/header.stories.tsx
@@ -1,12 +1,13 @@
-import { Story, Meta } from '@storybook/react';
+import  React  from 'react';
 import { Header, HeaderProps } from './header';
 
 export default {
   component: Header,
   title: 'Header',
-} as Meta;
+};
 
-const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+export const primary = () => {
+  const props: HeaderProps = {};
 
-export const Primary = Template.bind({});
-Primary.args = {};
+  return <Header />;
+}
